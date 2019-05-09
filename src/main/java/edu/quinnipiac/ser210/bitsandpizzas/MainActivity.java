b37f3dad4dbda9625d6eb7544464fdaae765c8c5
CTMaiorino <32141103+CTMaiorino@users.noreply.github.com>
2019-05-09 16:17:03 -0400

    commit - ch 8

---
+++ src/main/java/edu/quinnipiac/ser210/bitsandpizzas/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.quinnipiac.ser210.bitsandpizzas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, activity_order.class);
+                startActivity(intent);
+                return true;
+
+                default: return super.onOptionsItemSelected(item);
+        }
+    }
+}
