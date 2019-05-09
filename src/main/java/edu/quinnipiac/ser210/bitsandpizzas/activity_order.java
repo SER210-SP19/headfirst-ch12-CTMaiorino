9ed5273c4ec35310501bc39566ebe8ce231254d1
CTMaiorino <32141103+CTMaiorino@users.noreply.github.com>
2019-05-09 17:10:51 -0400

    commit - ch 12

--- src/main/java/edu/quinnipiac/ser210/bitsandpizzas/activity_order.java
+++ src/main/java/edu/quinnipiac/ser210/bitsandpizzas/activity_order.java
@@ -12,5 +12,23 @@ public class activity_order extends AppCompatActivity {
         setContentView(R.layout.activity_order);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickDone(View view) {
+        CharSequence text = "Your order has been updated";
+        int duration = Snackbar.LENGTH_SHORT;
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator), text, duration);
+        snackbar.setAction("Undo",
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast toast = Toast.makeText(OrderActivity.this, "Undone!",
+                                Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
+        snackbar.show();
     }
 }
